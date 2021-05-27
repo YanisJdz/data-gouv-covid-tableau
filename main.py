917a456aa893fe957087346c657f649b4e81a546
Yanis JEDRZEJCZAK <yanisjdz@outlook.fr>
2021-05-27 15:39:53 +0200

    Week filter iteration

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import glob
-import re
+import datetime
 
-region = 32 #Hauts de France
-age = 0 #Tout-âge
+region = 32  # Hauts de France
+age = 0  # Tout-âge
 
-#Ouverture des fichiers dans le répertoires et itération dessus
+# Ouverture des fichiers dans le répertoires et itération dessus
 for file_name in glob.glob("*.csv"):
 
     dataFrame = pd.read_csv(file_name, delimiter=";", keep_default_na=False)
@@ -17,24 +17,21 @@ for file_name in glob.glob("*.csv"):
     weekly_filtered_dataframe = pd.DataFrame(columns=dataFrame_out.columns)
 
     while index < dataFrame_out.shape[0]:
-        if dataFrame_out.iloc[index]['semaine'] == file_date: #Ici besoin de la condition où "file_date" changerait après chaque '.append()'
-            weekly_filtered_dataframe = weekly_filtered_dataframe.append(dataFrame_out.iloc[index])
-            #Ici une fois le .append on chercherait à modifier file_date pour rentrer dans condition
-            #Exemple : premieres dates = 2021-02-12-2021-02-18
-            #Après être rentré dans la condition if on changerait la valeur de file_date par = 2021-02-19-2021-02-25
-            #Tout ça pour ne selectionné qu'une ligne par semaine, en démarrant par les premières dates du fichier (ligne 16)
+        if dataFrame_out.iloc[index]['semaine'] == file_date:
+            try:
+                weekly_filtered_dataframe = weekly_filtered_dataframe.append(dataFrame_out.iloc[index])
+                dates = dataFrame_out.iloc[index]['semaine'].split('-')
+                next_start = datetime.datetime(int(dates[0]), int(dates[1]), int(dates[2]))
+                next_ending = datetime.datetime(int(dates[3]), int(dates[4]), int(dates[5]))
+                next_start = next_start + datetime.timedelta(days=7)
+                next_ending = next_ending + datetime.timedelta(days=7)
+                file_date = next_start.strftime("%Y") + '-' + next_start.strftime("%m") + '-' + next_start.strftime(
+                    "%d") + '-' + next_ending.strftime("%Y") + '-' + next_ending.strftime(
+                    "%m") + '-' + next_ending.strftime("%d")
+            except ValueError:
+                print('Data missing')
         index += 1
 
-    print(weekly_filtered_dataframe)
+# Réécriture dans un nouveau fichier csv
+    weekly_filtered_dataframe.to_csv('extracted_csv/extracted_'+file_name, header=True, index=False)
 
-#Réécriture dans un nouveau fichier csv
-    #dataFrame_out.to_csv('extracted_csv/extracted_'+file_name, header=True, index=False)
-
-
-#Essayer de récupérer la date de début des test (regexp) type "%d-%d-%d"
-#puis traduire en date et TU !!
-
-#Faire attention aux commentaires => Nommer correctement les fonctions peut permettre
-#d'éviter de faire des commentaires inutiles
-
-#Éviter les "magiques numbers" en nommant des variables ou autre
